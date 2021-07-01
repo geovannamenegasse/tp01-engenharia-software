d11c413514c130ea63c66d2de5207bfc35c2bf6f
Leandro <leandrodinizsilva9@gmail.com>
2021-07-01 18:58:43 -0300

    Adicionar turma / Lista turmas
    -Adicionado tabela turma no banco de dados

--- provasonline/turma/controller.py
+++ provasonline/turma/controller.py
@@ -1,8 +1,41 @@
+import re
+from provasonline import db, login_required
 from flask import Blueprint
 from flask import render_template, redirect, url_for, flash, request
+from provasonline.turma.models.Turma import Turma
+import json
 
 turma = Blueprint('turma', __name__, template_folder='templates')
 
 @turma.route("/listar_turmas", methods=["GET","POST"])
 def listar_turmas():
-    return render_template("listar_turmas.html")
+    turmas = Turma.query.all()
+    return render_template("listar_turmas.html", turmas=turmas)
+
+@turma.route("/cadastrar_turma", methods=["GET", "POST"])
+def cadastrar_turma():
+    if request.method == 'POST':
+
+        # descricao = request.form['prova']
+        nome = request.form['nome']
+        descricao = request.form['descricao']
+        turma     = Turma(descricao, nome)
+        db.session.add(turma)
+        db.session.commit()
+        flash("Prova cadastrada com sucesso")
+        return redirect(url_for('turma.listar_turmas'))
+    return render_template("cadastrar_turma.html")
+
+@turma.route("/adicionar_alunos", methods=["GET", "POST"])
+def adicionar_alunos():
+    id = request.args.get('id')
+    # if request.method == 'POST':
+        # id = request.form['id']
+        # nome = request.form['nome']
+        # nome = "%{}%".format(nome)
+        # alunos = Aluno.query.all(Turma.nome.like(search), Turma.email.like(search) )
+        # shows = [
+            # {"nome": 1, "email": "Sesaeme Street"},
+            # {"id": 2, "name": "Dora The Explorer"},
+        # ]
+    return render_template("adicionar_alunos.html", id = id)
--- provasonline/turma/models/Turma.py
+++ provasonline/turma/models/Turma.py
@@ -1,9 +1,12 @@
 from provasonline import db
-
 class Turma(db.Model):
-
     __tablename__ = 'turma'
-    id = db.Column(db.Integer, primary_key = True)
+    id              = db.Column(db.Integer, primary_key = True)
+    id_professor    = db.Column(db.Integer, db.ForeignKey('usuario.id', ondelete = 'CASCADE'), nullable = True)
+    descricao       = db.Column(db.Text, nullable = True)
+    nome            = db.Column(db.Text, nullable = True)
 
-    def __init__(self):
-        pass
+    def __init__(self, descricao, nome): #id_professor
+        #self.id_professor      = id_professor
+        self.descricao = descricao
+        self.nome = nome
